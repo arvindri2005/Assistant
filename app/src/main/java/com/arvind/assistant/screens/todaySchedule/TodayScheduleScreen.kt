7d635d44143ae0ae11b645a81597d4aedc58ad8a
Arvind <arvindri2005@gmail.com>
2024-12-02 09:44:12 +0530

    fix today schedule UI

--- app/src/main/java/com/arvind/assistant/screens/todaySchedule/TodayScheduleScreen.kt
+++ app/src/main/java/com/arvind/assistant/screens/todaySchedule/TodayScheduleScreen.kt
@@ -5,10 +5,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
@@ -29,6 +34,7 @@ import com.arvind.assistant.screens.todaySchedule.components.TodayScheduleListIt
 import java.time.LocalDate
 import java.time.LocalTime
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TodayScheduleScreen(
     todaySchedule :List<Pair<AttendanceRecordHybrid, AttendanceCounts>>,
@@ -40,50 +46,67 @@ fun TodayScheduleScreen(
         mutableStateOf(null)
     }
 
-    Surface(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        LazyColumn(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(
-                8.dp,
-                alignment = Alignment.Top
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Today's Schedule") },
+                modifier = Modifier
+                    .offset(y = (-40).dp)
             )
+        }
+    ) {it->
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+                .offset(y = (-40).dp)
+
         ) {
-            item{
-                AssistantButton(text = "Notification") {
-                    viewModel.setAlarm(todaySchedule[0].first.courseId, todaySchedule[0].first.courseName)
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
+                    .padding(horizontal = 5.dp),
+                verticalArrangement = Arrangement.spacedBy(
+                    8.dp,
+                    alignment = Alignment.Top
+                )
+            ) {
+//            item{
+//                AssistantButton(text = "Notification") {
+//                    viewModel.setAlarm(todaySchedule[0].first.courseId, todaySchedule[0].first.courseName)
+//                }
+//            }
+//            item {
+//                Spacer(modifier = Modifier.height(20.dp))
+//            }
+                items(
+                    todaySchedule,
+                ) { classItem ->
+                    TodayScheduleListItem(
+                        item = classItem.first,
+                        modifier = Modifier.padding(horizontal = 8.dp),
+                        onClick = {
+                            setClassSheetItem = classItem.first
+                        },
+                        attendanceCounts = classItem.second
+                    )
+                }
+                item {
+                    Spacer(modifier = Modifier.height(100.dp))
                 }
             }
-            item {
-                Spacer(modifier = Modifier.height(20.dp))
-            }
-            items(
-                todaySchedule,
-            ) { classItem ->
-                TodayScheduleListItem(
-                    item = classItem.first,
-                    modifier = Modifier.padding(horizontal = 8.dp),
-                    onClick = {
-                        setClassSheetItem = classItem.first
-                    },
-                    attendanceCounts = classItem.second
-                )
-            }
-            item {
-                Spacer(modifier = Modifier.height(100.dp))
-            }
+
         }
 
+        if (setClassSheetItem != null) {
+            SetClassStatusSheet(
+                todayCourseItem = setClassSheetItem!!,
+                onDismissRequest = { setClassSheetItem = null },
+                setClasStatus = { onSetClassStatus(setClassSheetItem!!, it) }
+            )
+        }
     }
 
-    if (setClassSheetItem != null) {
-        SetClassStatusSheet(
-            todayCourseItem = setClassSheetItem!!,
-            onDismissRequest = { setClassSheetItem = null },
-            setClasStatus = { onSetClassStatus(setClassSheetItem!!, it) }
-        )
-    }
+
 }
 
 @Preview
